c6f4a0fb1cf13112ef7b0df637b4e1f254e7a148
Ruthvikbr <ruthvikbr24@gmail.com>
2020-01-04 23:54:45 +0530

    Add Sign up alert dialog

--- app/src/main/java/com/example/projectx/LandingPageActivity.java
+++ app/src/main/java/com/example/projectx/LandingPageActivity.java
@@ -1,26 +1,51 @@
 package com.example.projectx;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LandingPageActivity extends AppCompatActivity {
-    private Button SignUp,SignIn;
+    private static final String TAG = "LandingPageActivity";
 
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private RadioGroup gender;
+     String Gender;
+    DatabaseReference user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        SignUp = findViewById(R.id.RegisterButton);
-        SignIn = findViewById(R.id.SignInButton);
-        SignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
 
     }
+
+
+
 }
