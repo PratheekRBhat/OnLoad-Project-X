b56b337eaa5b949f32089097e3a2acf44a67d262
Ruthvikbr <ruthvikbr24@gmail.com>
2020-01-10 15:50:16 +0530

    get current location

--- app/src/main/java/com/example/projectx/User_Activity.java
+++ app/src/main/java/com/example/projectx/User_Activity.java
@@ -1,29 +1,47 @@
 package com.example.projectx;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class User_Activity extends AppCompatActivity implements OnMapReadyCallback {
+public class User_Activity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnMarkerDragListener,
+        GoogleMap.OnMapLongClickListener,
+        GoogleMap.OnMarkerClickListener,
+        View.OnClickListener {
     private static final String TAG = User_Activity.class.getSimpleName();
     private GoogleMap mMap;
     private ProgressBar loader;
     private LinearLayout linearLayout;
     private Button DistressSignalButton;
     private String Gender;
-
+    private double longitude;
+    private double latitude;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +49,21 @@ public class User_Activity extends AppCompatActivity implements OnMapReadyCallba
         setContentView(R.layout.activity_user_);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
         loader = findViewById(R.id.loader);
         DistressSignalButton = findViewById(R.id.DistressSignal);
         Button attendingButton = findViewById(R.id.attendingButton);
         linearLayout = findViewById(R.id.mapLayout);
-        // loader.setVisibility(View.VISIBLE);
-
-
+        linearLayout.setVisibility(View.VISIBLE);
+        //loader.setVisibility(View.VISIBLE);
         attendingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,26 +77,104 @@ public class User_Activity extends AppCompatActivity implements OnMapReadyCallba
                 Toast.makeText(getApplicationContext(), "Help is on it's way", Toast.LENGTH_SHORT).show();
             }
         });
-
-
-
-
-
     }
-
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng sydney = new LatLng(12.9716, 77.5946);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Bangalore"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        LatLng india = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(india).title("Marker in India"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(india));
+        mMap.setOnMarkerDragListener(this);
+        mMap.setOnMapLongClickListener(this);
+    }
 
+    private void getCurrentLocation() {
+        mMap.clear();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if (location != null) {
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+            moveMap();
+        }
+    }
+
+    private void moveMap() {
+        LatLng latLng = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .draggable(true)
+                .title("Marker in India"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(18.04f));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.v(TAG, "view click event");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        getCurrentLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
 
     }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        mMap.addMarker(new MarkerOptions().position(latLng).draggable(true));
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Toast.makeText(User_Activity.this, "onMarkerClick", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        latitude = marker.getPosition().latitude;
+        longitude = marker.getPosition().longitude;
+        moveMap();
+
+    }
+
+    @Override
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
 
 
 
