11d006c816af2aad7aab1ec85859fb382b1587b2
Ruthvikbr <ruthvikbr24@gmail.com>
2020-02-27 16:55:27 +0530

    complaint registration form

--- app/src/main/java/com/example/projectx/complaintRegisterActivity.java
+++ app/src/main/java/com/example/projectx/complaintRegisterActivity.java
@@ -1,8 +1,11 @@
 package com.example.projectx;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 
 public class complaintRegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class complaintRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint_register);
-    }
+        Spinner spinner =  findViewById(R.id.complaintType);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.complaints, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+        }
 }
