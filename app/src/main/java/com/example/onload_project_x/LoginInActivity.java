ee65395c2153e723d44b72c7efd631168b00c8fb
Ruthvikbr <ruthvikbr24@gmail.com>
2020-02-28 23:24:45 +0530

    add login UI

--- app/src/main/java/com/example/onload_project_x/LoginInActivity.java
+++ app/src/main/java/com/example/onload_project_x/LoginInActivity.java
@@ -3,6 +3,9 @@ package com.example.onload_project_x;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class LoginInActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class LoginInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_in);
+        FloatingActionButton backButton = findViewById(R.id.loginPageBackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
