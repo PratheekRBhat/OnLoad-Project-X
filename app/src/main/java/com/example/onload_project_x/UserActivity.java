1b560e6985d76eb3574b58328c6cee7b7fe27402
0martel <pratheek.bht@gmail.com>
2020-02-29 03:34:40 +0530

    Update Map Activity with Location Worker

--- app/src/main/java/com/example/onload_project_x/UserActivity.java
+++ app/src/main/java/com/example/onload_project_x/UserActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
+import androidx.work.Data;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
 
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
@@ -27,6 +31,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -37,6 +42,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class UserActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -71,6 +78,7 @@ public class UserActivity extends AppCompatActivity implements OnMapReadyCallbac
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
@@ -78,16 +86,35 @@ public class UserActivity extends AppCompatActivity implements OnMapReadyCallbac
         loader = findViewById(R.id.loader);
 
         userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        WorkManager.getInstance(this).cancelAllWork();
+        if (mLocationPermissionGranted) {
+            startLocationWorker();
+        }
+    }
+
+    private void startLocationWorker(){
+        Data workerData = new Data.Builder().putString("uid", userID).build();
+        PeriodicWorkRequest workRequest = new PeriodicWorkRequest
+                .Builder(LocationWorker.class, 15, TimeUnit.MINUTES)
+                .setInputData(workerData).build();
+        WorkManager.getInstance(this).enqueue(workRequest);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        loader.setVisibility(View.VISIBLE);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setRotateGesturesEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+//        setMapStyle(mMap);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        getLocationPermission();
+
+        updateLocationUI();
+
+        getDeviceLocation();
     }
 
     private void getDeviceLocation(){
@@ -193,6 +220,17 @@ public class UserActivity extends AppCompatActivity implements OnMapReadyCallbac
         geoFire.setLocation(userID, new GeoLocation(latitude, longitude));
     }
 
+//    private void setMapStyle(GoogleMap map) {
+//        try {
+//            boolean success = map.setMapStyle(
+//                    MapStyleOptions.loadRawResourceStyle(
+//                            this, R.raw.map_style));
+//            if (!success) {
+//            }
+//        } catch (Resources.NotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
     @Override
     public void onBackPressed() {
         finishAffinity();
