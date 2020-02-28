18fd389ce555ce8edd81a265a4ab24074fcc3780
nehabalaji <nehabalaji10@gmail.com>
2020-02-29 01:16:30 +0530

    add sign up activity

--- app/src/main/java/com/example/onload_project_x/SignUpActivity.java
+++ app/src/main/java/com/example/onload_project_x/SignUpActivity.java
@@ -55,25 +55,25 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(TextUtils.isEmpty(emailET.getText().toString())){
-                    Toast.makeText(getApplicationContext(), "Enter Valid Email", Toast.LENGTH_SHORT);
+                    Toast.makeText(getApplicationContext(), "Enter Valid Email", Toast.LENGTH_SHORT).show();
                 }
                 else  if(TextUtils.isEmpty(nameET.getText().toString())){
-                    Toast.makeText(getApplicationContext(), "Enter Valid Name", Toast.LENGTH_SHORT);
+                    Toast.makeText(getApplicationContext(), "Enter Valid Name", Toast.LENGTH_SHORT).show();
                 }
                 else if(numberET.getText().toString().length() != 10){
-                    Toast.makeText(getApplicationContext(), "Enter Valid Phone Number", Toast.LENGTH_SHORT);
+                    Toast.makeText(getApplicationContext(), "Enter Valid Phone Number", Toast.LENGTH_SHORT).show();
                 }
                 else if(passwordET.getText().toString().length()!=6){
-                    Toast.makeText(getApplicationContext(), "Enter Valid Password", Toast.LENGTH_SHORT);
+                    Toast.makeText(getApplicationContext(), "Enter Valid Password", Toast.LENGTH_SHORT).show();
                 }
                 else if(genderRG.getCheckedRadioButtonId()==-1){
-                    Toast.makeText(getApplicationContext(), "Please Select Your Gender", Toast.LENGTH_SHORT);
+                    Toast.makeText(getApplicationContext(), "Please Select Your Gender", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    emailET.getText().toString();
-                    nameET.getText().toString();
-                    passwordET.getText().toString();
-                    numberET.getText().toString();
+                    email=emailET.getText().toString();
+                    name=nameET.getText().toString();
+                    password=passwordET.getText().toString();
+                    number=numberET.getText().toString();
                     int selectedID = genderRG.getCheckedRadioButtonId();
                     RadioButton selectedRadioButton = findViewById(selectedID);
                     gender = selectedRadioButton.getText().toString();
