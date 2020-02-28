e8984cc2de32001d7d63a9216720aae7cbf90529
Ruthvikbr <ruthvikbr24@gmail.com>
2020-02-28 22:30:41 +0530

    initial commit

---
+++ app/src/main/java/com/example/onload_project_x/LoginInActivity.java
@@ -0,0 +1,14 @@
+package com.example.onload_project_x;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_in);
+    }
+}
